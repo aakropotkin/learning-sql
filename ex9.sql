b2abee60c52050c111e5e6b1b6f623cebf976ffc
Alex Ameen <alex.ameen.tx@gmail.com>
2024-03-22 09:04:27 -0500

    ex 9.2 and fix 9.1

--- ex9.sql
+++ ex9.sql
@@ -13,14 +13,37 @@
 --
 -- -------------------------------------------------------------------------- --
 
-SELECT title FROM film
-WHERE ( SELECT category_id FROM film_category
-        WHERE film_category.film_id = film.film_id
-      ) = ( SELECT category_id FROM category WHERE name = 'Action' );
+SELECT title
+FROM film f
+WHERE f.film_id IN (
+  SELECT film_id
+  FROM film_category fc
+  INNER JOIN category c
+    ON c.category_id = fc.category_id
+  WHERE c.name = 'Action'
+);
+
 
 
 -- -------------------------------------------------------------------------- --
+--
+-- 9.2
+-- Consruct a query against the `film' table that uses a filter condition with a
+-- correlated subquery against the `category' table to find all action films
+-- ( `category.name' = 'Action' ).
+--
+-- -------------------------------------------------------------------------- --
 
+SELECT title
+FROM film f
+WHERE EXISTS (
+  SELECT 1
+  FROM film_category fc
+  INNER JOIN category c
+    ON c.category_id = fc.category_id
+  WHERE ( fc.film_id = f.film_id ) AND
+        ( c.name = 'Action' )
+);
 
 -- -------------------------------------------------------------------------- --
 --
