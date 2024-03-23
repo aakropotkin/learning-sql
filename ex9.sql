cdb6d1090dcdeecd11a205f9a06b7b982b20e6da
Alex Ameen <alex.ameen.tx@gmail.com>
2024-03-23 02:59:00 -0500

    finish ex9

--- ex9.sql
+++ ex9.sql
@@ -24,7 +24,6 @@ WHERE f.film_id IN (
 );
 
 
-
 -- -------------------------------------------------------------------------- --
 --
 -- 9.2
@@ -44,6 +43,39 @@ WHERE EXISTS (
   WHERE ( fc.film_id = f.film_id ) AND
         ( c.name = 'Action' )
 );
+
+-- -------------------------------------------------------------------------- --
+--
+-- 9.3
+-- Join the following query to a subquery against the `film_actor' table to show
+-- the level of each actor:
+--
+-- SELECT 'Hollywood Star' level, 30 min_roles, 99999 max_roles
+-- UNION ALL
+-- SELECT 'Prolific Actor' level, 20 min_roles, 29 max_roles
+-- UNION ALL
+-- SELECT 'Newcomer' level, 1 min_roles, 19 max_roles;
+--
+-- The subquery against the `film_actor' table should count the number of rows
+-- for each actor using `GROUP BY actor_id', and the count should be compared to
+-- the `min_roles'/`max_roles' columns to determine which level each actor
+-- belongs to.
+--
+-- -------------------------------------------------------------------------- --
+
+SELECT a.actor_id, a.first_name, a.last_name, l.level
+FROM actor a
+INNER JOIN ( SELECT 'Hollywood Star' level, 30 min_roles, 99999 max_roles
+             UNION ALL
+             SELECT 'Prolific Actor' level, 20 min_roles,    29 max_roles
+             UNION ALL
+             SELECT 'Newcomer'       level,  1 min_roles,    19 max_roles
+           ) l
+  ON ( SELECT count( * ) FROM film_actor fa
+       WHERE a.actor_id = fa.actor_id
+       GROUP BY actor_id
+     ) BETWEEN l.min_roles AND l.max_roles;
+
 
 -- -------------------------------------------------------------------------- --
 --
